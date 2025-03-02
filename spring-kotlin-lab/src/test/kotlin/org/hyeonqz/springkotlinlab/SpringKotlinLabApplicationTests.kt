29dd1ef2d24d34cc40645f179cee268ce9bfca7d
hkjin <wlsgusrb78@naver.com>
2025-03-02 14:55:08 +0900

    init

---
+++ spring-kotlin-lab/src/test/kotlin/org/hyeonqz/springkotlinlab/SpringKotlinLabApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.hyeonqz.springkotlinlab
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinLabApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
