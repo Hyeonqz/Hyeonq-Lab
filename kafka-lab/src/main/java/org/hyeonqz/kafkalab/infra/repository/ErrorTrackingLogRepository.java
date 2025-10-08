d265d9c91a25b511e1cdc606144f1949db762f24
hkjin <wlsgusrb78@naver.com>
2025-10-08 15:26:44 +0900

    :fire: Kafka related code and unnecessary entities removed

--- kafka-lab/src/main/java/org/hyeonqz/kafkalab/infra/repository/ErrorTrackingLogRepository.java
+++
@@ -1,7 +0,0 @@
-package org.hyeonqz.kafkalab.infra.repository;
-
-import org.hyeonqz.kafkalab.domain.entity.ErrorTrackingLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ErrorTrackingLogRepository extends JpaRepository<ErrorTrackingLog, Long> {
-}
--- kafka-lab/src/main/java/org/hyeonqz/kafkalab/infra/repository/PerformanceMetricsRepository.java
+++
@@ -1,7 +0,0 @@
-package org.hyeonqz.kafkalab.infra.repository;
-
-import org.hyeonqz.kafkalab.domain.entity.PerformanceMetrics;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PerformanceMetricsRepository extends JpaRepository<PerformanceMetrics, Long> {
-}
