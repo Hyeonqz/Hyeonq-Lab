44a94f5ef6f952c44c1f69b7f9c51cc657ec8fac
hkjin <wlsgusrb78@naver.com>
2026-03-23 22:43:51 +0900

    :fire: Remove java-lab project, including all source files, configurations, and resources for thread safety, sealed classes, and other Java experiments

--- java-lab/src/main/java/org/hyeonqz/java_lab/sealed/FXOrder.java
+++
@@ -1,9 +0,0 @@
-package org.hyeonqz.java_lab.sealed;
-
-import java.time.LocalDateTime;
-
-public sealed interface FXOrder permits MarketOrder, LimitOrder {
-	int units();
-	LocalDateTime sendAt();
-
-}
